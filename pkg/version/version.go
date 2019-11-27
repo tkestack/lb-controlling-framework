85f829fe6dcf7ff25e224845007c0bcb624d5a4d
ianlang <ianlang@tencent.com>
2019-11-27 12:25:28 +0800

    version sub-command

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -17,3 +17,63 @@
  */
 
 package version
+
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+	"k8s.io/klog"
+)
+
+var (
+	// GitVersion is semantic version.
+	GitVersion = "v0.0.0-master+$Format:%h$"
+	// GitCommit sha1 from git, output of $(git rev-parse HEAD)
+	GitCommit = "$Format:%H$"
+	// GitTreeState state of git tree, either "clean" or "dirty"
+	GitTreeState = ""
+	// BuildDate in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
+	BuildDate = "1970-01-01T00:00:00Z"
+)
+
+type Info struct {
+	GitVersion   string `json:"gitVersion"`
+	GitCommit    string `json:"gitCommit"`
+	GitTreeState string `json:"gitTreeState"`
+	BuildDate    string `json:"buildDate"`
+	GoVersion    string `json:"goVersion"`
+	Compiler     string `json:"compiler"`
+	Platform     string `json:"platform"`
+}
+
+func (i Info) String() string {
+	b, err := json.MarshalIndent(i, "", " ")
+	if err != nil {
+		return err.Error()
+	}
+	return string(b)
+}
+
+func NewCmdVersion() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "version",
+		Run: func(cmd *cobra.Command, args []string) {
+			klog.Info(Get().String())
+		},
+	}
+	return cmd
+}
+
+func Get() Info {
+	return Info{
+		GitVersion:   GitVersion,
+		GitCommit:    GitCommit,
+		GitTreeState: GitTreeState,
+		BuildDate:    BuildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+}
