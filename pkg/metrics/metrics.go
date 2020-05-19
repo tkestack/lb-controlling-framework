51e6f407e31a1a39e2f8e0e4938b27627c390ea7
ianlang <ianlang@tencent.com>
2020-05-19 18:58:05 +0800

    add key process & k8s op metrics

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -8,18 +8,28 @@ import (
 )
 
 var (
-	webhookCalls   *prometheus.CounterVec
-	webhookErrors  *prometheus.CounterVec
-	webhookFails   *prometheus.CounterVec
-	webhookLatency *prometheus.HistogramVec
-	pendingKeys    *prometheus.GaugeVec
-	workingKeys    *prometheus.GaugeVec
+	webhookCalls      *prometheus.CounterVec
+	webhookErrors     *prometheus.CounterVec
+	webhookFails      *prometheus.CounterVec
+	webhookLatency    *prometheus.HistogramVec
+	k8sOpLatency      *prometheus.HistogramVec
+	keyProcessLatency *prometheus.HistogramVec
+	pendingKeys       *prometheus.GaugeVec
+	workingKeys       *prometheus.GaugeVec
 )
 
 const (
 	labelKeyKind     = "key_kind"
 	labelDriverName  = "driver_name"
 	labelWebhookName = "webhook_name"
+	labelK8sOpObj    = "k8s_op_obj"
+	labelK8sOpType   = "k8s_op_type"
+	labelCRD         = "crd"
+
+	OpCreate       = "Create"
+	OpUpdate       = "Update"
+	OpUpdateStatus = "UpdateStatus"
+	OpDelete       = "Delete"
 )
 
 func init() {
@@ -52,6 +62,24 @@ func init() {
 				2.0, 3.0, 4.0, 5.0, 10.0, 20.0, 30.0},
 		},
 		[]string{labelDriverName, labelWebhookName})
+
+	k8sOpLatency = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "k8s_operation_latency",
+			Help: "k8s operation latencies in seconds",
+			Buckets: []float64{0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+				2.0, 3.0, 4.0, 5.0, 10.0, 20.0, 30.0},
+		},
+		[]string{labelK8sOpObj, labelK8sOpType})
+
+	keyProcessLatency = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "key_process_latency",
+			Help: "key process latencies in seconds",
+			Buckets: []float64{0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+				2.0, 3.0, 4.0, 5.0, 10.0, 20.0, 30.0},
+		},
+		[]string{labelCRD})
 
 	pendingKeys = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
@@ -96,6 +124,21 @@ func WebhookLatencyObserve(driverName, webhookName string, elapsed time.Duration
 	l := prometheus.Labels{
 		labelDriverName:  driverName,
 		labelWebhookName: webhookName,
+	}
+	webhookLatency.With(l).Observe(elapsed.Seconds())
+}
+
+func K8sOPLatencyObserve(obj, op string, elapsed time.Duration) {
+	l := prometheus.Labels{
+		labelK8sOpObj:  obj,
+		labelK8sOpType: op,
+	}
+	webhookLatency.With(l).Observe(elapsed.Seconds())
+}
+
+func KeyProcessLatencyObserve(crd string, elapsed time.Duration) {
+	l := prometheus.Labels{
+		labelCRD: crd,
 	}
 	webhookLatency.With(l).Observe(elapsed.Seconds())
 }
